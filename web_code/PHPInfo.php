9b7ed2305889e5f71f2a5b7905771ad6dd1a10fe
mingyi xie <84476708+UFXieLab@users.noreply.github.com>
2026-02-20 15:38:16 -0500

    Delete web_code directory

--- web_code/PHPInfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo "Who am I? " . exec('whoami') . "\n"; 
-echo "<p>";
-// echo "Current script owner: " . get_current_user() . "\n";
-
-phpinfo();
-?>
--- web_code/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbConfig = parse_ini_file(__DIR__ . "/DB.ini", true);
-$prodConfig = $dbConfig['prod'];
-$servername = $prodConfig['host'];
-$username   = $prodConfig['user'];
-$password   = $prodConfig['pass'];
-$dbname     = $prodConfig['db'];
-?>
